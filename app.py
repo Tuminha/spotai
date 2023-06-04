87d88b8c523d95ad2a7bcc94b4260728bcb1f936
Tuminha <83960151+Tuminha@users.noreply.github.com>
2023-06-04 23:19:58 +0200

    Main chnage API key to a new one

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os 
-os.environ['OPENAI_API_KEY'] = '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'
+os.environ['OPENAI_API_KEY'] = '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'
 
 import streamlit as st 
 from langchain.llms import OpenAI
