d61e8f1979f1d462a1fe9fd0525cbb2002eca66b
Tuminha <83960151+Tuminha@users.noreply.github.com>
2023-06-05 17:43:52 +0200

    Main Fixing some format

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os 
-
 import streamlit as st 
 from langchain.llms import OpenAI
 from langchain import PromptTemplate
@@ -34,7 +33,6 @@ with st.form(key='my_form'):
     selected_temperature = st.slider('Select the temperature mode', min_value=0, max_value=2, format="%d")
     
     submit_button = st.form_submit_button(label='Submit')
-
 
 if submit_button:
     # Validation
@@ -72,97 +70,65 @@ if submit_button:
             template='Write a compelling conclusion for a presentation on {main_topic} and {subtopic}.'
         )
 
-
     # Memory 
-    title_memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
-    intro_memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
-    overview_memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
-    topic_slide_memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
-    conclusion_memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
+    memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
 
     # GPT model
-    title_chain = LLMChain(prompt=title_template, llm=llm, memory=title_memory)
-    intro_chain = LLMChain(prompt=intro_template, llm=llm, memory=intro_memory)
-    overview_chain = LLMChain(prompt=overview_template, llm=llm, memory=overview_memory)
-    topic_slide_chain = LLMChain(prompt=topic_slide_template, llm=llm, memory=topic_slide_memory)
-    conclusion_chain = LLMChain(prompt=conclusion_template, llm=llm, memory=conclusion_memory)
+    title_chain = LLMChain(prompt=title_template, llm=llm, memory=memory)
+    intro_chain = LLMChain(prompt=intro_template, llm=llm, memory=memory)
+    overview_chain = LLMChain(prompt=overview_template, llm=llm, memory=memory)
+    topic_slide_chain = LLMChain(prompt=topic_slide_template, llm=llm, memory=memory)
+    conclusion_chain = LLMChain(prompt=conclusion_template, llm=llm, memory=memory)
 
-    # Wikipedia
+     # Initialize Wikipedia API wrapper
     wiki = WikipediaAPIWrapper()
 
-    # Show results
-    input_key_main_topic = main_topic
-    input_key_combined = f"{main_topic} {subtopic}"
-    duration = int(duration)  # Convert duration to an integer
+    # Progress bar
+    progress_bar = st.progress(0)
+    progress = 0.1
+    progress_bar.progress(progress)
 
-    progress_bar = st.progress(0)  # Initialize progress bar
+    # Fetch Wikipedia research for each of the topics and update the progress bar
+    main_topic_research = get_wiki_research(main_topic, progress)
+    progress += 0.1
 
-    # Perform separate queries to Wikipedia
-    wiki_research_main_topic = get_wiki_research(input_key_main_topic, 25/100)
-    wiki_research_combined = get_wiki_research(input_key_combined, 50/100)
+    subtopic_research = get_wiki_research(subtopic, progress)
+    progress += 0.1
 
-    # Generate content for each slide
-    slides = []
+    # Run the chains and update the progress bar
+    title = title_chain.run(main_topic=main_topic, subtopic=subtopic, duration=duration, audience=audience)
+    progress += 0.1
+    progress_bar.progress(progress)
 
-    # Title slide
-    title_slide = title_chain.run(main_topic=main_topic, subtopic=subtopic, duration=duration, audience=audience)
-    slides.append(("SLIDE 1: **{}**".format(title_slide.upper()), ''))
-    progress_bar.progress(60/100)  # Update the progress bar
+    intro = intro_chain.run(main_topic=main_topic, subtopic=subtopic)
+    progress += 0.1
+    progress_bar.progress(progress)
 
-    # Introduction slide
-    intro_slide = intro_chain.run(main_topic=main_topic, subtopic=subtopic)
-    slides.append(("SLIDE 2: **INTRODUCTION**", intro_slide))
-    progress_bar.progress(70/100)  # Update the progress bar
+    overview = overview_chain.run(main_topic=main_topic, subtopic=subtopic)
+    progress += 0.1
+    progress_bar.progress(progress)
 
-    # Overview slide
-    overview_slide = overview_chain.run(main_topic=main_topic, subtopic=subtopic)
-    slides.append(("SLIDE 3: **OVERVIEW**", overview_slide))
-    progress_bar.progress(75/100)  # Update the progress bar
+    topic_slide = topic_slide_chain.run(main_topic=main_topic, subtopic=subtopic, wikipedia_research=main_topic_research + "\n\n" + subtopic_research)
+    progress += 0.1
+    progress_bar.progress(progress)
 
-    # Topic slides
-    num_topic_slides = duration // 3  # 1 slide per 3 minutes of duration
-    for i in range(num_topic_slides):
-        topic_slide = topic_slide_chain.run(main_topic=main_topic, subtopic=subtopic, wikipedia_research=wiki_research_combined)
-        image_prompt = "Imagine a depiction of {main_topic} and {subtopic}. 4K, Realistic".format(main_topic=main_topic, subtopic=subtopic)
-        slides.append(("SLIDE {}: **{}**".format(i+4, topic_slide.upper()), topic_slide, image_prompt))
-        progress_bar.progress((75 + i*10/num_topic_slides) / 100)  # Update the progress bar
+    conclusion = conclusion_chain.run(main_topic=main_topic, subtopic=subtopic)
+    progress += 0.1
+    progress_bar.progress(progress)
 
-    # Conclusion slide
-    conclusion_slide = conclusion_chain.run(main_topic=main_topic, subtopic=subtopic)
-    slides.append(("SLIDE {}: **CONCLUSION**".format(num_topic_slides+4), conclusion_slide))
-    progress_bar.progress(90/100)  # Update the progress bar
+    # Show the results
+    st.success('Presentation generated successfully!')
+    st.header('Presentation Title')
+    st.write(title)
 
+    st.header('Introduction')
+    st.write(intro)
 
-    # Display the generated slides
-    for slide in slides:
-        st.markdown(slide[0])  # Slide title in uppercase and bold
-        st.write("• " + slide[1].replace(". ", ".\n• "))  # Bullet points for slide content
-        if len(slide) > 2:
-            st.write("Image Prompt: " + slide[2])  # Image prompt proposal
-        st.write("\n*Reference: Placeholder for bibliographic reference*\n")  # Placeholder for bibliographic reference
-        st.write("\n")  # Space between slides
+    st.header('Overview')
+    st.write(overview)
 
-    progress_bar.progress(100/100)  # Complete the progress bar
+    st.header('Main Topic Slide')
+    st.write(topic_slide)
 
-
-    with st.expander('Wikipedia Research - Main Topic'): 
-        st.info(wiki_research_main_topic)
-
-    with st.expander('Wikipedia Research - Combined Topic'): 
-        st.info(wiki_research_combined)
-
-        
-
-# Add custom CSS to the page
-st.markdown(
-    """
-    <style>
-    .stSlider { 
-        background-image: linear-gradient(to right, blue, red);
-        /* Add any other desired styling */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
+    st.header('Conclusion')
+    st.write(conclusion)
