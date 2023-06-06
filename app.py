4b43fa78d05382bd69cd1a36266b17fdb062cb60
Tuminha <83960151+Tuminha@users.noreply.github.com>
2023-06-06 09:16:37 +0200

    Main Fixed Streamlit.

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ if submit_button:
 
         wiki = WikipediaAPIWrapper()
 
-        col1, col2 = st.beta_columns(2)
+        col1, col2 = st.columns(2)
 
         with col1:
             st.header('Slide Title')
