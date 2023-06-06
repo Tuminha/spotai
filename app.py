b2eeae155013c3f0247c03de48ce3bf3514cfcb0
Tuminha <83960151+Tuminha@users.noreply.github.com>
2023-06-06 09:53:12 +0200

    Main Reformatting app

--- app.py
+++ app.py
@@ -31,9 +31,26 @@ with st.form(key='my_form'):
         'Leticia Sala Mode': 0.5,
         'Robles Mode': 0.9
     }
-    selected_temperature = st.selectbox('Select the temperature mode', list(temperature_options.keys()))
+    selected_temperature = st.selectbox('Select the temperature mode. The different modes represent different expected output', list(temperature_options.keys()))
     
     submit_button = st.form_submit_button(label='Submit')
+
+    # ADD: New memory class to keep track of previous slide content
+    class ExtendedConversationBufferMemory(ConversationBufferMemory):
+        def __init__(self, input_key, memory_key, slide_content_key):
+            super().__init__(input_key, memory_key)
+            self.slide_content_key = slide_content_key
+
+        def update_memory(self, input_data, output_data):
+            super().update_memory(input_data, output_data)
+            if self.slide_content_key in input_data:
+                self.memory[self.slide_content_key] = input_data[self.slide_content_key]
+
+        def get_memory(self):
+            memory = super().get_memory()
+            if self.slide_content_key in self.memory:
+                memory[self.slide_content_key] = self.memory[self.slide_content_key]
+            return memory
 
 
 if submit_button:
@@ -47,38 +64,44 @@ if submit_button:
         llm = OpenAI(api_key=openai_api_key, temperature=temperature)
 
         # Prompt templates
+        # CHANGE: Varying the prompt templates to provide more context and guidance
+
         title_template = PromptTemplate(
             input_variables=['main_topic', 'subtopic', 'duration', 'audience'], 
-            template='Write me a presentation title about {main_topic} and provide some details about the {subtopic} for a {duration}-minute presentation for {audience}.'
+            template='Compose a presentation title that encompasses {main_topic} and elaborates on the {subtopic} for a presentation lasting {duration} minutes, targeted at {audience}.'
         )
 
         intro_template = PromptTemplate(
             input_variables=['main_topic', 'subtopic'],
-            template='Write an engaging introduction about {main_topic} and provide some details about the {subtopic}.'
+            template='Craft an engaging introduction that familiarizes the audience with the topic of {main_topic} and elucidates the significance of the {subtopic} in this context.'
         )
 
         overview_template = PromptTemplate(
             input_variables=['main_topic', 'subtopic'],
-            template='Provide an overview of the topics to be covered in the presentation on {main_topic} and {subtopic}.'
+            template='Outline the key subjects to be addressed in the presentation on {main_topic} and {subtopic}, providing a roadmap for the audience.'
         )
 
         topic_slide_template = PromptTemplate(
-            input_variables=['main_topic', 'subtopic', 'wikipedia_research'],
-            template='Create a slide about {main_topic} and {subtopic} based on this research: {wikipedia_research}.'
+            input_variables=['main_topic', 'subtopic', 'wikipedia_research', 'previous_slide_content'],
+            template='Generate a slide focused on {main_topic} and {subtopic} drawing from this research: {wikipedia_research}. The slide should seamlessly follow the content of the previous slide: {previous_slide_content}'
         )
 
         conclusion_template = PromptTemplate(
             input_variables=['main_topic', 'subtopic'],
-            template='Write a compelling conclusion for a presentation on {main_topic} and {subtopic}.'
+            template='Compose a compelling conclusion that encapsulates the key points discussed about {main_topic} and {subtopic}, and leaves the audience with a clear take-home message.'
         )
 
 
-    # Memory 
-    title_memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
-    intro_memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
-    overview_memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
-    topic_slide_memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
-    conclusion_memory = ConversationBufferMemory(input_key='main_topic', memory_key='chat_history')
+
+    # CHANGE: Use the new memory class for the chains
+    title_memory = ExtendedConversationBufferMemory(input_key='main_topic', memory_key='chat_history', slide_content_key='previous_slide_content')
+    intro_memory = ExtendedConversationBufferMemory(input_key='main_topic', memory_key='chat_history', slide_content_key='previous_slide_content')
+    overview_memory = ExtendedConversationBufferMemory(input_key='main_topic', memory_key='chat_history', slide_content_key='previous_slide_content')
+    topic_slide_memory = ExtendedConversationBufferMemory(input_key='main_topic', memory_key='chat_history', slide_content_key='previous_slide_content')
+    conclusion_memory = ExtendedConversationBufferMemory(input_key='main_topic', memory_key='chat_history', slide_content_key='previous_slide_content')
+
+    
+
 
     # GPT model
     title_chain = LLMChain(prompt=title_template, llm=llm, memory=title_memory)
@@ -119,34 +142,42 @@ if submit_button:
     slides.append(("SLIDE 3: **OVERVIEW**", overview_slide))
     progress_bar.progress(75/100)  # Update the progress bar
 
-    # Topic slides
+        # Topic slides
     num_topic_slides = duration // 3  # 1 slide per 3 minutes of duration
+
+    # CHANGE: Include the content of the previous slide when running the chain
+    previous_slide_content = ""
     for i in range(num_topic_slides):
-        topic_slide = topic_slide_chain.run(main_topic=main_topic, subtopic=subtopic, wikipedia_research=wiki_research_combined)
+        topic_slide = topic_slide_chain.run(main_topic=main_topic, subtopic=subtopic, wikipedia_research=wiki_research_combined, previous_slide_content=previous_slide_content)
         image_prompt = "Imagine a depiction of {main_topic} and {subtopic}. 4K, Realistic".format(main_topic=main_topic, subtopic=subtopic)
         slides.append(("SLIDE {}: **{}**".format(i+4, topic_slide.upper()), topic_slide, image_prompt))
         progress_bar.progress((75 + i*10/num_topic_slides) / 100)  # Update the progress bar
+        previous_slide_content = topic_slide  # update previous_slide_content
 
     # Conclusion slide
     conclusion_slide = conclusion_chain.run(main_topic=main_topic, subtopic=subtopic)
     slides.append(("SLIDE {}: **CONCLUSION**".format(num_topic_slides+4), conclusion_slide))
     progress_bar.progress(90/100)  # Update the progress bar
 
-
     # Display the generated slides
     for slide in slides:
-        st.markdown(slide[0])  # Slide title in uppercase and bold
-        st.write("• " + slide[1].replace(". ", ".\n• "))  # Bullet points for slide content
-        if len(slide) > 2:
-            st.write("Image Prompt: " + slide[2])  # Image prompt proposal
-        st.write("\n*Reference: Placeholder for bibliographic reference*\n")  # Placeholder for bibliographic reference
-        st.write("\n")  # Space between slides
+        slide_title = slide[0]
+        slide_content = slide[1]
+        slide_image_prompt = slide[2] if len(slide) > 2 else None
+        slide_content_formatted = "• " + slide_content.replace(". ", ".\n• ")  # Bullet points for slide content
+
+        st.markdown(slide_title)
+        st.markdown(slide_content_formatted)
+        if slide_image_prompt:
+            st.markdown(f"**Image Prompt:** {slide_image_prompt}")
+        st.markdown("\n_Reference: Placeholder for bibliographic reference_\n")
+        st.markdown("---")
 
     progress_bar.progress(100/100)  # Complete the progress bar
-
 
     with st.expander('Wikipedia Research - Main Topic'): 
         st.info(wiki_research_main_topic)
 
     with st.expander('Wikipedia Research - Combined Topic'): 
         st.info(wiki_research_combined)
+
